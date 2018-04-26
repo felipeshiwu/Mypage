c4f6d74d3a6f4f16f0b73e3b20492ed3e6b930d8
Felipe Wu <fsiw15@inf.ufpr.br>
2018-04-26 17:25:46 -0300

    vai
    Signed-off-by: Felipe Wu <fsiw15@inf.ufpr.br>

--- web/poligonos/script.js
+++ web/poligonos/script.js
@@ -1,59 +1,168 @@
 var canvas = document.querySelector('canvas');
 console.log(canvas);
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-
-// c.fillStyle = "green";
-// c.fillRect(100, 100, 100, 100)
-// c.fillRect(400, 100, 100, 100)
-// c.fillRect(300, 300, 100, 100)
-//
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = "red";
-// c.stroke();
-//
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI*2, false);
-// c.stroke();
-//
-// for (var i = 0; i < 100; i++) {
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, Math.PI*2, false);
-//     c.stroke();
-//
-// }
+var leftClick = false;
+var rightClick = false;
+var centerX = window.innerWidth / 2;
+var centerY = window.innerHeight / 2;
 
 
-var x = Math.random() * innerWidth;
-var y = x+200;
-var dx = 20;
-var dy = 20;
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.moveTo(x, y);
-    c.lineTo(250+x, y-200);
-    c.strokeStyle = "red";
-    c.stroke();
-    if (x+250 > innerWidth || x < 0) {
-        dx = -dx;
+
+function Line(x0, y0, xf, yf) {
+    this.x0 = x0;
+    this.y0 = y0;
+    this.xf = xf;
+    this.yf = yf;
+    this.width = Math.abs(xf - x0);
+    this.height = Math.abs(yf - y0);
+    if (this.x0 < this.xf)
+      this.centerX = x0 + this.width/2;
+    else 
+      this.centerX = xf + this.width/2;
+    if (this.y0 < this.yf)
+      this.centerY = y0 + this.height/2;
+    else 
+      this.centerY = yf + this.height/2;
+
+
+    this.draw = function(selected) {
+        c.beginPath();
+        c.lineWidth = 3;
+        c.moveTo(this.x0, this.y0);
+        c.lineTo(this.xf, this.yf);
+        if (selected) {
+            c.strokeStyle = "red";
+        } else {
+            c.strokeStyle = "black";
+        }
+        c.stroke();
     }
-    if (y-200 > innerHeight || y < 0) {
-        dy = -dy;
-    }
-    x+= dx;
-    y+= dy;
+
 }
 
-var 
+var mx = undefined;
+var my = undefined;
+
+//events
+window.addEventListener('resize', resizeCanvas, false);
+window.addEventListener('mouseup', mouseUP, false);
+window.addEventListener('mousedown', mouseDW, false);
+document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    }, false);
+
+onmousemove = function (event) {
+    mx = event.clientX;
+    my = event.clientY;
+}
+
+
+var lines = [];
+lines.push(new Line(centerX-100,centerY,centerX+100,centerY));
+var selectedLine = null;
+
+function animate() {
+
+    moveMouse(event);
+    drawLines();
+    requestAnimationFrame(animate);
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var centerX = window.innerWidth / 2;
+    var centerY = window.innerHeight / 2;
+    animate();
+}
+
+function mouseDW(event) {
+    if (event.button == 2) {
+        rightClick = true;
+    } else if (event.button == 1) {
+        leftClick = true;
+    }
+}
+
+function mouseUP() {
+    rightClick = false;
+    leftClick = false;
+}
+
+function moveMouse(event) {
+    lines[0].vx += mx-lines[0].vx;
+    lines[0].vy += my-lines[0].vy;
+}
+
+function drawLines() {
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    for (var i = lines.length - 1; i >= 0; i--) {
+        if (selectedLine == i)
+            lines[i].draw(true);
+        else
+            lines[i].draw(false);
+    }
+
+}
+
+function selectLine() {
+    if (leftClick && selectedLine == null) {
+        for (var i = lines.length - 1; i >= 0; i--) {
+            if (mx >= lines[i].centerX - 10 &&
+                mouseX <= lines[i].centerX + 10 &&
+                my >= lines[i].centerY - 10 &&
+                my <= lines[i].centerY + 10) {
+                    selectedLine = i;
+                    movingEntireLine = true;
+                    break;
+            } else if (mx >= lines[i].x0 - 20 &&
+                       mx <= lines[i].x0 + 20 &&
+                       my >= lines[i].y0 - 20 &&
+                       my <= lines[i].y0 + 20) {
+                        selectedLine = i;
+                        movingLineHead = true;
+                        break;
+            } else if (mx >= lines[i].xf - 20 &&
+                       mx <= lines[i].xf + 20 &&
+                       my >= lines[i].yf - 20 &&
+                       my <= lines[i].yf + 20) {
+                        selectedLine = i;
+                        movingLineTail = true;
+                        break;
+            }
+        }
+    } else if (rightClick) {
+        for (var i = lines.length - 1; i >= 0; i--) {
+            if ((my >= lines[i].y0 && my <= lines[i].yf) ||
+                (my >= lines[i].yf && my <= lines[i].y0)) {
+                    if (mx >= lines[i].x0 && mx <= lines[i].xf) {
+                        lines[lines.length] = new Line(mx,my,lines[i].xf,lines[i].yf);
+                        lines[i].xf = mx;
+                        lines[i].yf = my;
+                        lines[i].width = Math.abs(lines[i].xf - lines[i].x0);
+                        lines[i].height = Math.abs(lines[i].yf - lines[i].y0);
+                        lines[i].centerX = lines[i].x0 + lines[i].width/2;
+                        lines[i].centerY = lines[i].y0 + lines[i].height/2;      
+                        rightClick = 0;
+                        break;
+                    } else if (mx < lines[i].x0 && mx > lines[i].xf) {
+                        lines[lines.length] = new Line(mx,my,lines[i].x0,lines[i].y0);
+                        lines[i].x0 = mx;
+                        lines[i].y0 = my;
+                        lines[i].width = Math.abs(lines[i].xf - lines[i].x0);
+                        lines[i].height = Math.abs(lines[i].yf - lines[i].y0);
+                        lines[i].centerX = lines[i].xf + lines[i].width/2;
+                        lines[i].centerY = lines[i].yf + lines[i].height/2;       
+                        rightClick = 0;
+                        break;        
+                    }
+            }
+        }
+    }
+}
+
 
 animate();
